397203b836d7cf48293afbd47697e7433520a4ce
Matthew Lui <gamma032@gmail.com>
2019-10-15 10:58:06 +1100

    Optimise and refactor

--- proj2.pl
+++ proj2.pl
@@ -1,35 +1,50 @@
+% Purpose: Solve a grid-based math puzzle with as search algorithm.
+%
+% Uses Prolog's constrain logic programming library to transform the game's
+% rules into a combinatorial problem for its solution. We use this library
+% for arithmatic, membership and combinatorial constraints.
+%
+% Any cell in the grid must be unique in its row and column, be 1-9, and
+% its row and column values must sum or multiply to its associated header
+% value. Values down the diagonal must be equal.
+
+% Libraries
 :- ensure_loaded(library(clpfd)).
 
 % Finds the solution to the puzzle
 puzzle_solution(Rows) :-
-    line_rule(Rows), % make this a rule to avoid duplication
+    once(Rows), % Only look for one solution
+    line_rule(Rows),
     transpose(Rows, Columns),
     line_rule(Columns),
     diagonal_rule(Rows),
-    maplist(label, Rows).
+    maplist(label, Rows). % Make the values ground
 
+% drops a row and column
+inner_grid(Grid, InnerGrid) :-
+    drop_head(Grid, RightGrid),
+    transpose(RightGrid, Grid2),
+    drop_head(Grid2, InnerGrid).
+
+top_left(Grid, Value) :-
+    nth0(0, Grid, FirstLine),
+    nth0(0, FirstLine, Value).
+    
 % rule: check diagonal
 % start by removing labels, then initialise check with top-left value
-diagonal_rule(Rows) :-
-    drop_head(Rows, Values),
-    transpose(Values, Columns), % swap to remove row
-    drop_head(Columns, Values2),
-    nth0(0, Values2, FirstLine), % the column
-    nth0(0, FirstLine, Value),
-    validate_diagonal(Values2, Value).
+diagonal_rule(Grid) :-
+    inner_grid(Grid, InnerGrid),
+    top_left(InnerGrid, Value),
+    validate_diagonal(InnerGrid, Value).
 
 % recursive. removes row and column then checks value
 validate_diagonal([[Value]], Value). % mactches to bottom-right
-validate_diagonal(Lines, Value) :-
-    drop_head(Lines, Values),
-    transpose(Values, Columns),
-    drop_head(Columns, Values2),
-    nth0(0, Values2, FirstLine), % the column
-    nth0(0, FirstLine, TopLeft),
+validate_diagonal(Grid, Value) :-
+    inner_grid(Grid, InnerGrid),
+    top_left(InnerGrid, TopLeft),
     TopLeft #= Value,
-    validate_diagonal(Values2, Value).
+    validate_diagonal(InnerGrid, Value).
     
-
 % Rule: check rows
 line_rule([_|Line]) :-
     validate_lines(Line).
@@ -48,14 +63,10 @@ validate_line([Heading|Values]) :-
     multiply(Values, 1, Heading)).
 
 % list and acc are bound
-multiply([], Acc, Product) :-
-    Acc #= Product.
+multiply([], Acc, Acc).
 multiply([X|Xs], Acc, Product) :-
-    Acc2 #= X * Acc,
-    multiply(Xs, Acc2, Product).
-    
-    
+    New_acc #= X * Acc,
+    multiply(Xs, New_acc, Product).
     
 drop_head([_|Tail], Tail).
 
-test(A,B) :- A =:= B; true.
