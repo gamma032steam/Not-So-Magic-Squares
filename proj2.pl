2e7a32d546360751b52123ee0b4633db6c859ede
gamma032steam <gamma032@gmail.com>
2019-10-15 11:43:09 +1100

    Swap to prolog-style documentation

--- proj2.pl
+++ proj2.pl
@@ -1,3 +1,4 @@
+% Author: Matthew Lui 993333
 % Purpose: Solve a grid-based math puzzle with as search algorithm.
 %
 % Uses Prolog's constrain logic programming library to transform the game's
@@ -8,10 +9,13 @@
 % its row and column values must sum or multiply to its associated header
 % value. Values down the diagonal must be equal.
 
+% Algorithms are fun!
+
 % Libraries
 :- ensure_loaded(library(clpfd)).
 
-% Finds the solution to the puzzle
+% puzzle_solution(+Rows)
+% Enforce constraints with CLPFD and search for a single ground solution
 puzzle_solution(Rows) :-
     once(Rows), % Only look for one solution
     line_rule(Rows),
@@ -20,53 +24,72 @@ puzzle_solution(Rows) :-
     diagonal_rule(Rows),
     maplist(label, Rows). % Make the values ground
 
-% drops a row and column
-inner_grid(Grid, InnerGrid) :-
-    drop_head(Grid, RightGrid),
-    transpose(RightGrid, Grid2),
-    drop_head(Grid2, InnerGrid).
+%%% LINE RULE
 
-top_left(Grid, Value) :-
-    nth0(0, Grid, FirstLine),
-    nth0(0, FirstLine, Value).
-    
-% rule: check diagonal
-% start by removing labels, then initialise check with top-left value
-diagonal_rule(Grid) :-
-    inner_grid(Grid, InnerGrid),
-    top_left(InnerGrid, Value),
-    validate_diagonal(InnerGrid, Value).
+% line_rule(+Grid)
+% Removes the label and enforces the line rules.
+line_rule([_|Lines]) :-
+    validate_lines(Lines).
 
-% recursive. removes row and column then checks value
-validate_diagonal([[Value]], Value). % mactches to bottom-right
-validate_diagonal(Grid, Value) :-
-    inner_grid(Grid, InnerGrid),
-    top_left(InnerGrid, TopLeft),
-    TopLeft #= Value,
-    validate_diagonal(InnerGrid, Value).
-    
-% Rule: check rows
-line_rule([_|Line]) :-
-    validate_lines(Line).
-
-% check the rules one at a time
+% validate_lines(+Grid)
+% Recursively validates the grid, one line at a time.
 validate_lines([]).
 validate_lines([Line|Lines]) :-
     validate_line(Line),
     validate_lines(Lines).
 
-% validate 1 row
+% validate_line(+Line)
+% Applies the line rules to Line, in that values are distinct, 1-9 and
+% either sum or multiply to their label.
 validate_line([Heading|Values]) :-
-    all_distinct(Values), % we can use the new func that chops off 1 row and col to do this in a diff rule
+    all_distinct(Values),
     Values ins 1..9,
     (sum(Values, #=, Heading);
     multiply(Values, 1, Heading)).
 
-% list and acc are bound
+% multiply(+List, +Acc, -Product)
+% multiply(+List, +Acc, +Product)
+% Elements of List multiply together to from Product, starting from Acc. 
 multiply([], Acc, Acc).
 multiply([X|Xs], Acc, Product) :-
     New_acc #= X * Acc,
     multiply(Xs, New_acc, Product).
-    
-drop_head([_|Tail], Tail).
 
+%%% DIAGONAL RULE
+
+% diagonal_rule(+Grid)
+% Removes the first row and column of Grid and unifies the leading diagonal.
+diagonal_rule(Grid) :-
+    inner_grid(Grid, InnerGrid),
+    top_left(InnerGrid, Value),
+    validate_diagonal(InnerGrid, Value).
+
+% validate_diagonal(+Grid, +Value)
+% Recursively unifies the leading diagonal of Grid one element at a time,
+% removing a row and column on each step until the bottom-right element is
+% encountered
+validate_diagonal([[Value]], Value).
+validate_diagonal(Grid, Value) :-
+    inner_grid(Grid, InnerGrid),
+    top_left(InnerGrid, TopLeft),
+    TopLeft #= Value,
+    validate_diagonal(InnerGrid, Value).
+
+% inner_grid(+Grid, -InnerGrid)
+% Drops the first row and column of a 2D matrix. InnerGrid is transposed
+% relative to Grid.
+inner_grid(Grid, InnerGrid) :-
+    drop_head(Grid, RightGrid),
+    transpose(RightGrid, Grid2),
+    drop_head(Grid2, InnerGrid).
+
+% top_left(+Grid, +Value)
+% top_left(+Grid, -Value)
+% Value is the term in the first row and column of Grid.
+top_left(Grid, Value) :-
+    nth0(0, Grid, FirstLine),
+    nth0(0, FirstLine, Value).
+
+% drop_head(+List, -Tail)
+% Tail is the elements remaining in List after its first item is removed.
+drop_head([_|Tail], Tail).
